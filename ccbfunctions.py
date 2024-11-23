fb3780ba10ad02170d84c3369b95ac19476bac99
bibliobaloney <110797927+bibliobaloney@users.noreply.github.com>
2024-11-22 17:08:10 -0800

    end of week updates

--- ccbfunctions.py
+++ ccbfunctions.py
@@ -324,7 +324,8 @@ reasonsmenu = {1: "Bad-faith conduct", 2: "Failure to amend", 3: "3 tries and st
                12: "Settlement; prejudice pending", 
                13: "Settlement; dismissed with prejudice", 14: "Settlement; dismissed without prejudice", 
                15: "Copyright registration application abandoned", 16: "Service was not valid", 
-               17: "No copyright registration or application", 18: None}
+               17: "No copyright registration or application", 
+               18: "Unsuitability: statute of limitations", 19: None}
 
 # Get a human to read the dismissal order. Returns tuple (HumanRead, WithPrejudice, SettlementMention, Reason1, Reason2)
 def humandismissalinfo(documentnum, ordertext):
@@ -338,7 +339,7 @@ def humandismissalinfo(documentnum, ordertext):
         print(item, reasonsmenu[item])
     reasoninput1 = input('Enter a reason number from the menu: ')
     reason1 = reasonsmenu[int(reasoninput1)]
-    reasoninput2 = input('Enter the second reason if there is one, 18 (None) if not: ')
+    reasoninput2 = input('Enter the second reason if there is one, 19 (None) if not: ')
     reason2 = reasonsmenu[int(reasoninput2)]
     dismissalinfo = (documentnum, 1, prej, settlement, reason1, reason2)
     return dismissalinfo
